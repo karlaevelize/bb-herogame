b65a95090fbb7b17363d38b4d0b8d7411eaec680
karlaevelize <k.evelize@gmail.com>
2019-11-09 12:47:41 +0100

    Hero Object added to JS

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,19 @@
 // Write your JS here
+
+// declare a variable hero and assign it an object
+//Hero object should have a name property. The value of name should be a string.
+//Hero object should have a name property. The value of name should be a string
+//Hero object should have an inventory property. The value of inventory should be an empty array
+//Hero object should have an health property. The value health should be the number 10
+//Hero object should have a weapon property. The value of weapon is an object with a type property which is a string and damage property which is 2
+const hero = {
+    name: "Harry Potter",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "",
+        damage: 2
+    }
+}
+
